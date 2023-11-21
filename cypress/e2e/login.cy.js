32c0f545ba049c4952803b30b97dd6fa36cea740
Surajit Sarkar <suro@Surajits-MacBook-Pro-2.local>
2023-11-21 21:08:21 +0530

    Handling various web ui elements

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+///<reference types="cypress" />
+
+describe('Orange HRM Login', () => {
+
+  beforeEach("Open Browser",()=>{
+    cy.log("Opening browser")
+    cy.visit('/web/index.php/auth/login')
+  })
+
+
+
+  it('valid login', () => {
+    cy.get("[name='username']").clear().type('Admin');
+    cy.get("[name='password']").clear().type('admin123');
+    cy.get("[type=submit").click();
+    cy.get("[class*='topbar-header-breadcrumb-module']").should("have.text","Dashboard")
+    
   })
 })
---
+++ cypress/e2e/web-ui-elements.spec.cy.js
@@ -0,0 +1,63 @@
+///<reference types="cypress" />
+
+describe('Handle Web UI Elements', () => {
+
+    beforeEach("Launch Application",()=>{
+      cy.log("Opening browser")
+      cy.visit('https://www.globalsqa.com/samplepagetest/')
+    })
+  
+  
+  
+    it('Form fill', () => {
+    //File Upload
+      cy.get("[name='file-553']").selectFile("cypress/fixtures/sampleImage.jpeg");
+
+      //Text field
+      cy.get("[name='g2599-name']").clear().type("Surajit Sarkar");
+      cy.get("#g2599-email").clear().type("test@test.com");
+      cy.get("#g2599-website").clear().type("http://myweb.com");
+
+      //Dropdown
+      cy.get("#g2599-experienceinyears").select("10+")
+
+      //Checkbox
+      cy.get("[type='checkbox'][value='Automation Testing']").should('not.be.checked').check();
+      cy.get("[type='checkbox'][value='Functional Testing']").should('not.be.checked').check();
+
+      //Radio Button
+      cy.get("[type='radio'][value='Graduate']").should('not.be.checked').check();
+
+      //Alert Box
+      cy.get("button[onclick='myFunction()']").click().then(()=>{
+        cy.on("window:alert",(msg)=>{
+            should(msg).equal("Do you really fill rest of the form?");
+        });
+        cy.on("window:confirm",()=>{
+            return true
+        });
+        cy.on("window:alert",(msg)=>{
+            should(msg).equal("Good Luck. Go for it");
+        });
+        cy.on("window:confirm",()=>{
+            return true
+        });
+      });
+
+      //Text area
+      cy.get("#contact-form-comment-g2599-comment").clear().type("Interacting with Cypress Text area...");
+
+      //button
+      cy.get("[type='submit']").click();
+
+      //validate the submssion message
+      //should use case
+      cy.get("[id='contact-form-2599']>h3").should('have.text',"Message Sent (go back)")
+
+      //expect use case
+      cy.get("[id='contact-form-2599']>h3").then((ele)=>{
+        expect(ele.text()).equal("Message Sent (go back)")
+      })
+      
+    })
+  })
